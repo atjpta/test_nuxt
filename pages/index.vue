08a0f3ab6ca26442bf87b7949401cb9a23d4f1c3
atjpta <caoducan351@gmail.com>
2022-12-14 17:39:27 +0700

    add code

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <div>
+ day là trang home
+   </div>
+   <div>
+    hiwerghpr
+   </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/test/2.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>2222222222222 {{test}} aegfwerg</div>
+</template>
+
+<script setup>
+const { data: test } = await useFetch('https://api-wblog.vercel.app/api/users');
+</script>
+
+<style>
+
+</style>
